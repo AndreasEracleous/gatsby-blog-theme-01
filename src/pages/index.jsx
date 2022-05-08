2d22d21ca33cd091bcae513ab41ba659554939dd
Andreas <andrew.eracleous@gmail.com>
2022-05-08 19:01:18 +0300

    Updated files

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,20 @@
-import { graphql } from "gatsby";
-import React from "react";
-import Seo from "../components/Seo";
-import PostListLayout from "../components/PostListLayout";
-import SubscribeForm from "../components/SubscribeForm";
-import SocialMedia from "../components/SocialMedia";
-import { PAGE_CONTENT } from "../config/constants";
+import { graphql } from "gatsby"
+import React from "react"
+import Seo from "../components/Seo"
+import PostListLayout from "../components/PostListLayout"
+import SubscribeForm from "../components/SubscribeForm"
+import SocialMedia from "../components/SocialMedia"
+import { PAGE_CONTENT } from "../config/constants"
 
 const IndexPage = ({ data, location }) => {
-  const { nodes } = data.allMdx;
-  const title = PAGE_CONTENT["title"];
-  const seoTitle = PAGE_CONTENT["seo_title"];
-  const occupation = PAGE_CONTENT["occupation"];
-  const blogTitle = PAGE_CONTENT["blog_title"];
-  const blogDesc = PAGE_CONTENT["blog_description"];
-  const socialMediaText = PAGE_CONTENT["social_media"];
-  const subscribeURL = PAGE_CONTENT["subscribe_url"];
+  const { nodes } = data.allMdx
+  const title = PAGE_CONTENT["title"]
+  const seoTitle = PAGE_CONTENT["seo_title"]
+  const occupation = PAGE_CONTENT["occupation"]
+  const blogTitle = PAGE_CONTENT["blog_title"]
+  const blogDesc = PAGE_CONTENT["blog_description"]
+  const socialMediaText = PAGE_CONTENT["social_media"]
+  const subscribeURL = PAGE_CONTENT["subscribe_url"]
 
   const intro = (
     <h1 className="max-w-5xl tracking-tight text-black text-5xl font-bold lg:text-6xl xl:text-7xl">
@@ -22,14 +22,14 @@ const IndexPage = ({ data, location }) => {
       <br />
       <span className="text-blue">{occupation}</span>.
     </h1>
-  );
+  )
 
   const welcome = (
     <>
       <h2 className="font-light text-4xl mt-5">{blogTitle}</h2>
       <p className="mt-4 max-w-3xl text-2xl">{blogDesc}</p>
     </>
-  );
+  )
 
   return (
     <>
@@ -43,10 +43,10 @@ const IndexPage = ({ data, location }) => {
       />
       <PostListLayout data={nodes} />
     </>
-  );
-};
+  )
+}
 
-export default IndexPage;
+export default IndexPage
 
 export const query = graphql`
   query SITE_INDEX_QUERY {
@@ -71,4 +71,4 @@ export const query = graphql`
       }
     }
   }
-`;
+`
