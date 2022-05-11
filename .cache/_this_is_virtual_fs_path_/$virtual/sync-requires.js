6c07809bdee3fa1ff3360ad880618a031e2e5f27
Andreas <andrew.eracleous@gmail.com>
2022-05-11 10:55:28 +0300

    Updated package file

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -4,6 +4,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/andreaseracleous/Documents/Work Environment/Personal/Themes/gatsby-blog-theme-01/.cache/dev-404-page.js")),
   "component---src-pages-404-jsx": preferDefault(require("/Users/andreaseracleous/Documents/Work Environment/Personal/Themes/gatsby-blog-theme-01/src/pages/404.jsx")),
   "component---src-pages-index-jsx": preferDefault(require("/Users/andreaseracleous/Documents/Work Environment/Personal/Themes/gatsby-blog-theme-01/src/pages/index.jsx")),
   "component---src-templates-blog-post-jsx": preferDefault(require("/Users/andreaseracleous/Documents/Work Environment/Personal/Themes/gatsby-blog-theme-01/src/templates/BlogPost.jsx"))
