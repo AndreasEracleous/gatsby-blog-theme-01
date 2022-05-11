6656225006f5f80af11dd824cc4b7a18edaa745e
Andreas <andrew.eracleous@gmail.com>
2022-05-11 11:57:58 +0300

    Updated files

--- gatsby-node.js
+++ gatsby-node.js
@@ -66,3 +66,12 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [path.resolve(__dirname, "src"), "node_modules"],
+      alias: { $components: path.resolve(__dirname, "src/components") },
+    },
+  })
+}
